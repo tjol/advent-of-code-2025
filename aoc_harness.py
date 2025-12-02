6528efa6543be72ac58e65706c23f2b689e6ff68
Thomas Jollans <tjol@tjol.eu>
2025-12-02 20:08:37 +0100

    Day 2

--- aoc_harness.py
+++ aoc_harness.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 def puzzle_main(*solvers):
     if len(sys.argv) == 1:
@@ -15,5 +16,9 @@ def puzzle_main(*solvers):
     
     input_data = in_file.read()
     for solver in solvers:
+        t1 = time.perf_counter()
         result = solver(input_data)
+        t2 = time.perf_counter()
         print(result)
+        duration = t2 - t1
+        print(f'time: {duration*1000.0:.03f} ms')
---
+++ day02.py
@@ -0,0 +1,60 @@
+import itertools
+
+def day02part1(input: str):
+    ranges = [
+        tuple(map(int, range_str.split("-"))) for range_str in input.strip().split(",")
+    ]
+    max_val = max(r[1] for r in ranges)
+
+    invalid_sum = 0
+    for i in itertools.count(1):
+        invalid_num = int(2 * str(i))
+        if invalid_num > max_val:
+            break
+        for n, m in ranges:
+            if n <= invalid_num <= m:
+                invalid_sum += invalid_num
+                break
+
+    return invalid_sum
+
+def day02part2(input: str):
+    ranges = [
+        tuple(map(int, range_str.split("-"))) for range_str in input.strip().split(",")
+    ]
+    max_val = max(r[1] for r in ranges)
+
+    invalid_nums = set()
+    for i in itertools.count(1):
+        any_tested = False
+        for k in itertools.count(2):
+            invalid_num = int(k * str(i))
+            if invalid_num > max_val:
+                break
+
+            any_tested = True
+
+            for n, m in ranges:
+                if n <= invalid_num <= m:
+                    invalid_nums.add(invalid_num)
+                    break
+        if not any_tested:
+            break
+
+    return sum(invalid_nums)
+
+
+TEST_INPUT_1 = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+
+
+def test_part1():
+    assert day02part1(TEST_INPUT_1) == 1227775554
+
+def test_part2():
+    assert day02part2(TEST_INPUT_1) == 4174379265
+
+
+if __name__ == "__main__":
+    from aoc_harness import puzzle_main
+
+    puzzle_main(day02part1, day02part2)
