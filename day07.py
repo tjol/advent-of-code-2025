3a70831f7b8e381dab5ab4f837fa248b17ee1f03
Thomas Jollans <tjol@tjol.eu>
2025-12-07 11:31:32 +0100

    day 7 part 2

--- day07.py
+++ day07.py
@@ -18,6 +18,27 @@ def day07part1(input_: str):
     return splits
 
 
+def day07part2(input_: str):
+    world = [*map(list, input_.strip().splitlines())]
+    timeline_map = [[1 if c == "S" else 0 for c in row] for row in world]
+
+    splits = 0
+
+    for y in range(1, len(world)):
+        for x in range(len(world[y])):
+            above = timeline_map[y - 1][x]
+            obstacle = world[y][x]
+            if above == 0:
+                continue
+            if obstacle == "^":
+                timeline_map[y][x - 1] += above
+                timeline_map[y][x + 1] += above
+            else:
+                timeline_map[y][x] += above
+
+    return sum(timeline_map[-1])
+
+
 TEST_INPUT = """
 .......S.......
 ...............
@@ -42,7 +63,11 @@ def test_part1():
     assert day07part1(TEST_INPUT) == 21
 
 
+def test_part2():
+    assert day07part2(TEST_INPUT) == 40
+
+
 if __name__ == "__main__":
     from aoc_harness import puzzle_main
 
-    puzzle_main(day07part1)
+    puzzle_main(day07part1, day07part2)
